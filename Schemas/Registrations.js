800e577efce4b25dd3adb9a73a0419cf225aa1cd
karun <karun.m.2005@gmail.com>
2025-05-07 19:45:29 +0530

    a simple start of the backend with the basics

---
+++ Schemas/Registrations.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const participants = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    usn: {
+      type: String,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const registrationSchema = mongoose.Schema({
+  team_name: {
+    type: String,
+    required: true,
+  },
+  student_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "students",
+    required: true,
+  },
+  event_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "events",
+    required: true,
+  },
+  participants: [participants],
+  registration_date: {
+    type: Date,
+    default: Date.now,
+  },
+  verification: {
+    type: String,
+    enum: ["awaiting", "verified", "rejected"],
+    default: "awaiting",
+  },
+});
+
+registrationSchema.index({ student_id: 1, event_id: 1 }, { unique: true });
+registrationSchema.index({ team_name: 1, event_id: 1 }, { unique: true });
+
+const Registrations = mongoose.model("registrations", registrationSchema);
+module.exports = Registrations;
