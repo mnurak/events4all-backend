2e9ddb943bae89d60075ae583e9abde4327ae919
Karun M <161990287+mnurak@users.noreply.github.com>
2025-05-14 21:07:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 const port = process.env.PORT;
 
-const corsOptions = {
-  origin: 'https://events4all.vercel.app',
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "auth-token"],
-  credentials: true
-};
+app.use(cors({
+  origin: 'https://events4all.vercel.app', // your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // if you're using cookies or auth headers
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
